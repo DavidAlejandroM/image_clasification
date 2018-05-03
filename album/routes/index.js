6624539a232dd1ef02f1f7283aafddc0ae30a578
David Alejandro Marin Alzate <jandro240@gmail.com>
2018-05-03 13:26:21 -0500

    moviendo archivos

--- album/routes/index.js
+++ album/routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const pythonShell = require('python-shell');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  pythonShell.run('../../test.py', function (err) {
+
+    if (err) throw err;
+    console.log(Foto);
+  });
   res.render('index', { title: 'Express' });
 });
 
