2e6819b8aa2fca60572fcc0accf0a48496bd0736
David Alejandro Marin Alzate <jandro240@gmail.com>
2018-05-19 18:08:00 -0500

    app

--- utils/orderUtil.js
+++ utils/orderUtil.js
@@ -7,8 +7,10 @@ module.exports = {
         let esPrimera = false;
 
         clases.forEach(clase => {
+
             let foto = fotoSiguiente(clase,currentPosition,fotos);
-            console.log('primera foto ************');
+            //console.log('primera foto ************',currentPosition);
+            
             if(foto && !esPrimera){
                 fotosOrdenadas.push(foto);
                 fotos = deleteFoto(foto,fotos);
@@ -17,9 +19,9 @@ module.exports = {
         });
         let count = 0;
         clases.forEach(clase => {
-            
+            //console.log(clase)
             let foto = fotoSiguiente(clase,fotosOrdenadas[fotosOrdenadas.length - 1],fotos);
-            console.log(`primera foto ${count}************`);
+            //console.log(`primera foto ${count}************`);
             
             while(foto){
                 
@@ -31,8 +33,12 @@ module.exports = {
             
         });
 
-        if(fotos.length > 0){
-            fotosOrdenadas = fotosOrdenadas.concat(fotos);
+        //console.log(fotosOrdenadas[fotosOrdenadas.length - 1], fotos.length)
+        while(fotos.length > 0){
+            let foto = fotoSiguiente('noclass',fotosOrdenadas[fotosOrdenadas.length - 1],fotos);
+            //console.log(foto)
+            fotosOrdenadas.push(foto);
+            fotos = deleteFoto(foto,fotos);    
         }
 
         return fotosOrdenadas;
@@ -51,13 +57,18 @@ function deleteFoto(foto, fotos){
 
 function fotoSiguiente(clase, fotoOrdenada,fotos){
     let primeraFoto;
+    if(clase==='noclass'){
+        //console.log(fotoOrdenada)
+    }
     if(fotos && fotos.length > 0 && fotoOrdenada){
         let lat1 = parseFloat(fotoOrdenada.latitud);
         let lng1 = parseFloat(fotoOrdenada.longitud);
         let lat2 = parseFloat(fotos[0].latitud);
         let lng2 = parseFloat(fotos[0].longitud);
 
-        let distanciaActual = pUtil.getDistanceMts(lat1, lng1, lat2, lng2);
+        console.log('lat1:', lat1,' lng1:', lng1, ' lat2:', lat2, ' lng2:', lng2);
+
+        let distanciaActual = 15000e6;
         let distanciaMin = distanciaActual;
         
         fotos.forEach( foto => {
@@ -65,6 +76,12 @@ function fotoSiguiente(clase, fotoOrdenada,fotos){
             
             if(foto.clase == clase && distanciaMin >= distanciaActual){
                 distanciaMin = distanciaActual;
+                console.log(distanciaMin, lat1);
+                primeraFoto = foto;
+            }
+            else if(clase == 'noclass' && distanciaMin >= distanciaActual){
+                distanciaMin = distanciaActual;
+                console.log(distanciaMin, lat1);
                 primeraFoto = foto;
             }
         });
