19a88c4b85e788fc2ccc7ddf094611e3db1f98c0
David Alejandro Marin Alzate <jandro240@gmail.com>
2018-05-19 19:08:21 -0500

    cree los providers de la app

--- app_album/src/app/app.module.ts
+++ app_album/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpProvider } from '../providers/http/http';
+import { PackageProvider } from '../providers/package/package';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    HttpProvider,
+    PackageProvider
   ]
 })
 export class AppModule {}
